8cf0bcfda5efec23ed66433455637a120c6b197f
Mythili473 <mythilimallu15@gmail.com>
2024-09-11 15:47:09 +0530

    Add files via upload

---
+++ Human Pose Estimation.py
@@ -0,0 +1,90 @@
+!pip install opencv-python
+
+!pip install matplotlib
+
+import cv2 as cv
+import matplotlib.pyplot as plt
+import time
+
+net = cv.dnn.readNetFromTensorflow("graph_opt.pb")
+
+inWidth = 368
+inHeight = 368
+thr = 0.2
+
+BODY_PARTS = {"Nose": 0, "Neck": 1, "RShoulder": 2, "RElbow": 3, "RWrist": 4,
+              "LShoulder": 5, "LElbow": 6, "LWrist": 7, "RHip": 8, "RKnee": 9,
+              "RAnkle": 10, "LHip": 11, "LKnee": 12, "LAnkle": 13, "REye": 14,
+              "LEye": 15}
+
+POSE_PAIRS = [["Neck", "RShoulder"], ["Neck", "LShoulder"], ["RShoulder", "RElbow"],
+              ["RElbow", "RWrist"], ["LShoulder", "LElbow"], ["LElbow", "LWrist"],
+              ["Neck", "RHip"], ["RHip", "RKnee"], ["RKnee", "RAnkle"], ["Neck", "LHip"],
+              ["LHip", "LKnee"], ["LKnee", "LAnkle"], ["Neck", "Nose"], ["Nose", "REye"], ["Nose", "LEye"]]
+
+# Function to perform pose estimation on a frame
+def pos_estimation(frame):
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+
+    net.setInput(cv.dnn.blobFromImage(frame, 1.0, (inWidth, inHeight), (127.5, 127.5, 127.5), swapRB=True, crop=False))
+
+    out = net.forward()
+    out = out[:, :19, :, :]
+
+    assert (len(BODY_PARTS) <= out.shape[1])
+
+    points = []
+
+    for i in range(len(BODY_PARTS)):
+        heatMap = out[0, i, :, :]
+
+        _, conf, _, point = cv.minMaxLoc(heatMap)
+        x = (frameWidth * point[0]) / out.shape[3]
+        y = (frameHeight * point[1]) / out.shape[2]
+
+        points.append((int(x), int(y)) if conf > thr else None)
+
+    for pair in POSE_PAIRS:
+        partFrom = pair[0]
+        partTo = pair[1]
+        assert (partFrom in BODY_PARTS)
+        assert (partTo in BODY_PARTS)
+
+        idFrom = BODY_PARTS[partFrom]
+        idTo = BODY_PARTS[partTo]
+
+        if points[idFrom] and points[idTo]:
+            cv.line(frame, points[idFrom], points[idTo], (0, 255, 0), 3)
+            cv.ellipse(frame, points[idFrom], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+            cv.ellipse(frame, points[idTo], (3, 3), 0, 0, 360, (0, 0, 255), cv.FILLED)
+
+    t, _ = net.getPerfProfile()
+    freq = cv.getTickFrequency() / 1000
+    cv.putText(frame, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
+    return frame
+
+# Capture video from the webcam
+cap = cv.VideoCapture(0)
+
+start_time = time.time()
+elapsed_time = 0
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Perform pose estimation on the frame
+    estimated_frame = pos_estimation(frame)
+
+    # Display the frame
+    cv.imshow('Pose Estimation', estimated_frame)
+
+    # Check for 'p' key press to quit
+    if cv.waitKey(1) & 0xFF == ord('p'):
+        break
+
+# Release the video capture object and close all windows
+cap.release()
+cv.destroyAllWindows()
